f741cc85a95d965a8903a6a6edfa0c5640018bb2
nofitarahma <novikarahma542@gmail.com>
2026-04-02 09:30:36 +0700

    tambahan nofita

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,27 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityRequirement;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+            $openApi->components->securitySchemes['kelompok-header'] = SecurityScheme::apiKey('header', 'X-Kelompok');
+            $openApi->components->securitySchemes['category-header'] = SecurityScheme::apiKey('header', 'X-Category-Access');
+
+            $openApi->security[] = new SecurityRequirement(['kelompok-header' => []]);
+            $openApi->security[] = new SecurityRequirement(['category-header' => []]);
+        });
     }
 }
